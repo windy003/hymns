9c164d4bbb94cab7ccc82a2baaa637290f51dc31
windy003 <928920471@qq.com>
2026-09-17 11:40:13 +0800

    调整

--- NeedLogin/run_hymns_nli.cmd
+++ NeedLogin/run_hymns_nli.cmd
@@ -1,1 +1,1 @@
-start /b ""  "D:\files\using\Web\hymns\NeedLogin\.venv\Scripts\pythonw.exe" pythonw "D:\files\using\Web\hymns\NeedLogin\app.py"
+start /b ""  "D:\files\using\Web\hymns\NeedLogin\.venv\Scripts\pythonw.exe" "D:\files\using\Web\hymns\NeedLogin\app.py"
